223185d11f78f7e555f6eb026aca99c5be236cc0
HGieselmann <henrik@henrikgieselmann.com>
2018-05-17 18:32:27 +0200

    Example 3-3 added
    - Added Example 3-3
    - improved Example 1-10

--- Assets/Scripts/Oscillation/Example_3_3.cs
+++ Assets/Scripts/Oscillation/Example_3_3.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 
 
-public class Example_1_10 : MonoBehaviour {
+public class Example_3_3 : MonoBehaviour {
 	
-	public Example_1_10_Mover MyMover;
-	public GameObject mySphere;
+	public Example_3_3_Mover MyMover;
+	private GameObject mySphere;
 	
 	Ray mousePosRay = new Ray();
 	public Camera myCam;
@@ -19,8 +19,9 @@ public class Example_1_10 : MonoBehaviour {
 	void Start ()
 	{
 		
-		MyMover = new Example_1_10_Mover();
-		mySphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		MyMover = new Example_3_3_Mover();
+		mySphere = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		mySphere.transform.localScale = new Vector3(.6F,.2F,.2f);
 		//mySphere = GetComponent<Transform>();
 
 	}
@@ -36,6 +37,7 @@ public class Example_1_10 : MonoBehaviour {
 		MyMover.UpdatePosition(pos.point);
 		MyMover.CheckEdges();
 		mySphere.transform.position = new Vector3(MyMover.location.x, MyMover.location.y, 0f);
+		mySphere.transform.rotation = Quaternion.Euler(MyMover.rotDir);
 
 
 	}
---
+++ Assets/Scripts/Oscillation/Example_3_3_Mover.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Example_3_3_Mover : MonoBehaviour {
+	
+	public static int CSizeX = 16;
+	public static int CSizeY = 9;
+	
+
+	public Vector3 location;
+	private Vector3 velocity;
+	private Vector3 acceleration;
+	private Vector3 mousePos;
+	private Vector3 dir;
+
+	public Vector3 rotDir;
+	
+	
+	public float topspeed = 1f;
+	
+	
+	
+
+	// Call me Captn Constructor
+	public Example_3_3_Mover()
+	{
+		;
+		
+		location = new Vector3(UnityEngine.Random.Range(0,CSizeX), UnityEngine.Random.Range(0, CSizeY), 0f);
+		velocity = new Vector3(0f, 0f, 0f);
+		acceleration = new Vector3( 0, 0, 0);
+			
+	}
+
+	public void UpdatePosition(Vector3 _mousePos)
+	{
+
+		mousePos = _mousePos;
+		dir = mousePos - location;
+		dir = dir.normalized;
+		dir *= 0.1f;
+		acceleration = dir;
+		
+		
+		//acceleration = ReturnRandomVector();
+		velocity += acceleration * Time.deltaTime;
+		velocity = Vector3.ClampMagnitude(velocity, topspeed); // Yes, PVector.limit is more beautiful...
+		location += velocity ;
+
+		float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+		rotDir = new Vector3(0, 0, angle);
+
+
+	}
+
+	public void CheckEdges()
+	{
+		if (location.x > CSizeX)
+		{
+			location.x = 0;
+		} else if (location.x < 0)
+		{
+			location.x = CSizeX;
+		}
+
+		if (location.y > CSizeY)
+		{
+			location.y = 0;
+		} else if (location.y < 0)
+		{
+			location.y = CSizeY;
+		}
+	}
+
+
+}
