1ad28d4abeaa523433b3d743cc33d44320327c56
HGieselmann <henrik@henrikgieselmann.com>
2018-05-18 12:39:33 +0200

    Added Examples on Oscillation
    - Added Example 3-3
    - Added Example 3-4
    - Added Example 3-5
    - Added Example 3-6
    - Added Example 3-7

---
+++ Assets/Scripts/Oscillation/Example_3_5.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example_3_5 : MonoBehaviour
+{
+
+	private float CSizeX = 16;
+	private float CSizeY = 9;
+	private GameObject sphere;
+	private float x;
+
+	public float period = 120f;
+	public float amplitude = 1000f;
+	
+	
+	// Use this for initialization
+	void Start () {
+		sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 x = amplitude * Mathf.Cos(Mathf.PI*2 * Time.frameCount / period); // This is bad implementation based on frameCount
+		 //x = amplitude * Mathf.Cos(Time.time); // Simpler based on time, smooth motion because it is not framerate dependent
+		sphere.transform.position = new Vector3(x + CSizeX/2, CSizeY/2, 0);
+		
+		
+		
+	}
+}
---
+++ Assets/Scripts/Oscillation/Example_3_6.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example_3_6 : MonoBehaviour
+{
+
+	private float CSizeX = 16;
+	private float CSizeY = 9;
+	
+	private GameObject sphere;
+	public float angularVel = 5f; // Bigger Value because we use Time.deltaTime;
+	public float amplitude = 8;
+	private float angle;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		angle += angularVel * Time.deltaTime;
+		float x = amplitude * Mathf.Cos(angle+Time.time/2);
+		float y = (amplitude/2) * Mathf.Cos(angle+45+Time.time);
+		
+		sphere.transform.position = new Vector3(x + CSizeX/2, y+CSizeY/2, 0f );
+		
+	}
+}
