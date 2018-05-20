a2d5fc9ea8549936a394035bdfd7e1a0891afcac
HGieselmann <henrik@henrikgieselmann.com>
2018-05-20 16:31:48 +0200

    Added Example 3_11
    - Added Example 3-11

---
+++ Assets/Scripts/Oscillation/Example_3_11.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example_3_11 : MonoBehaviour
+{
+	private float CSizeX = 16;
+	private float CSizeY = 9;
+
+	private LineRenderer line;
+	private Spring spring;
+	public Bob bob;
+
+	private GameObject sphere;
+	// Use this for initialization
+	void Start ()
+	{
+		line = gameObject.AddComponent<LineRenderer>();
+		line.widthMultiplier = 0.2f;
+		sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		bob = new Bob(8, 2f);
+		spring = new Spring(8,9,5);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 gravity = new Vector3(0, 2, 0);
+		bob.applyForce(gravity);
+		spring.connect(bob);
+		bob.update();
+		sphere.transform.position = bob.position;
+		
+		line.SetPosition(0,spring.origin);
+		line.SetPosition(1, bob.position);
+		
+		
+
+	}
+}
