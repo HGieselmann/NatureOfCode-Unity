457781e4e81c754e1dcb3f12d1bdd57ccb52c2a7
HGieselmann <henrik@henrikgieselmann.com>
2018-05-20 18:50:32 +0200

    Added Level Manager and implemeted it as a prefab
    - Added Level Manager and implemeted it as a prefab

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    private Scene scene;
+
+    public void Update()
+    {
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void NextScene()
+    {
+        int currentscene = SceneManager.GetActiveScene().buildIndex;
+        
+        SceneManager.LoadScene(currentscene + 1);
+    }
+    
+    public void PrevScene()
+    {
+        int currentscene = SceneManager.GetActiveScene().buildIndex;
+        
+        SceneManager.LoadScene(currentscene - 1);
+    }
+    public void Home()
+        {
+            int currentscene = scene.buildIndex;
+            
+            SceneManager.LoadScene(0);
+        }
+    
+}
